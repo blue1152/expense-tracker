1fa7bad8427bac4703a1c4b5e4e873e3dcf40420
blue1152 <bluememory1152@gmail.com>
2019-12-17 01:35:39 +0800

    final 12/17 ver.

--- routes/home.js
+++ routes/home.js
@@ -13,101 +13,58 @@ router.get("/", authenticated, (req, res) => {
 });
 // 篩選框
 router.get("/category/home", authenticated, (req, res) => {
-  Record.aggregate(
-    [
-      {
-        $match: {
-          $and: [{ userId: req.user._id }, { category: "家居物業" }]
-        }
-      },
-      { $group: { _id: "$category", total: { $sum: "$amount" } } }
-    ],
-    (err, records) => {
-      if (err) return console.error(err);
-      return res.render("index", { records });
-    }
-  ).then(res => console.log(res));
-});
-router.get("/category/traffic", authenticated, (req, res) => {
-  Record.aggregate(
-    [
-      {
-        $match: {
-          $and: [{ userId: req.user._id }, { category: "交通出行" }]
-        }
-      },
-      { $group: { _id: "$category", total: { $sum: "$amount" } } }
-    ],
-    (err, records) => {
-      if (err) return console.error(err);
-      return res.render("index", { records });
-    }
-  ).then(res => console.log(res));
-});
-router.get("/category/relax", authenticated, (req, res) => {
-  Record.aggregate(
-    [
-      {
-        $match: {
-          $and: [{ userId: req.user._id }, { category: "休閒娛樂" }]
-        }
-      },
-      { $group: { _id: "$category", total: { $sum: "$amount" } } }
-    ],
-    (err, records) => {
-      if (err) return console.error(err);
-      return res.render("index", { records });
-    }
-  ).then(res => console.log(res));
-});
-router.get("/category/food", authenticated, (req, res) => {
-  Record.aggregate(
-    [
-      {
-        $match: {
-          $and: [{ userId: req.user._id }, { category: "餐飲食品" }]
-        }
-      },
-      { $group: { _id: "$category", total: { $sum: "$amount" } } }
-    ],
-    (err, records) => {
-      if (err) return console.error(err);
-      return res.render("index", { records });
-    }
-  ).then(res => console.log(res));
-});
-router.get("/category/others", authenticated, (req, res) => {
-  Record.aggregate(
-    [
-      {
-        $match: {
-          $and: [{ userId: req.user._id }, { category: "其他" }]
-        }
-      },
-      { $group: { _id: "$category", total: { $sum: "$amount" } } }
-    ],
-    (err, records) => {
-      if (err) return console.error(err);
-      return res.render("index", { records });
-    }
-  ).then(res => console.log(res));
-});
-
-//搜尋框
-router.get("/search", authenticated, (req, res) => {
-  const keyword = req.query.keyword;
-  const keywordRegex = new RegExp(keyword, "i"); //正規表達式
   Record.find(
     {
-      $and: [{ userId: req.user._id }],
-      $or: [
-        { name: { $regex: keywordRegex, $options: "$i" } },
-        { category: { $regex: keywordRegex } }
-      ]
+      $and: [{ userId: req.user._id }, { category: "家居物業" }]
     },
     (err, records) => {
       if (err) return console.error(err);
-      return res.render("index", { records, keyword });
+      return res.render("index", { records: records }); // 將資料傳給 index
+    }
+  );
+});
+
+router.get("/category/traffic", authenticated, (req, res) => {
+  Record.find(
+    {
+      $and: [{ userId: req.user._id }, { category: "交通出行" }]
+    },
+    (err, records) => {
+      if (err) return console.error(err);
+      return res.render("index", { records: records }); // 將資料傳給 index
+    }
+  );
+});
+router.get("/category/relax", authenticated, (req, res) => {
+  Record.find(
+    {
+      $and: [{ userId: req.user._id }, { category: "休閒娛樂" }]
+    },
+    (err, records) => {
+      if (err) return console.error(err);
+      return res.render("index", { records: records }); // 將資料傳給 index
+    }
+  );
+});
+router.get("/category/food", authenticated, (req, res) => {
+  Record.find(
+    {
+      $and: [{ userId: req.user._id }, { category: "餐飲食品" }]
+    },
+    (err, records) => {
+      if (err) return console.error(err);
+      return res.render("index", { records: records }); // 將資料傳給 index
+    }
+  );
+});
+router.get("/category/others", authenticated, (req, res) => {
+  Record.find(
+    {
+      $and: [{ userId: req.user._id }, { category: "其他" }]
+    },
+    (err, records) => {
+      if (err) return console.error(err);
+      return res.render("index", { records: records }); // 將資料傳給 index
     }
   );
 });
